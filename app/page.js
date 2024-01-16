f899ca7135e3f287ddb190179e9ae7409f27228a
Yeasir Arafat <yeasirar@gmail.com>
2024-01-16 23:52:55 +0600

    sidebar

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Login from '../components/auth/login'
 import Signup from '../components/auth/signup'
+import Sidebar from '@/components/sidebar/sidebar'
 
 export default function Home() {
   var flag = false;
   return (
     <main>
       {flag? <Login /> : <Signup />}
+      <Sidebar />
     </main>
   )
 }
