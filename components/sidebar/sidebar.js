da411b982336c1bce5898215e1448a1745bddc11
Yeasir Arafat <yeasirar@gmail.com>
2024-01-17 02:07:40 +0600

    sidebar updated

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -1,70 +1,119 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { CalendarIcon, ChartBarIcon, FolderIcon, HomeIcon, InboxIcon, UsersIcon } from '@heroicons/react/24/outline'
+"use client"
+import { Fragment, useEffect, useState } from "react";
+import { Menu, Popover, Transition } from "@headlessui/react";
+import {
+  ChatBubbleLeftEllipsisIcon,
+  CodeBracketIcon,
+  EllipsisVerticalIcon,
+  EyeIcon,
+  FlagIcon,
+  HandThumbUpIcon,
+  MagnifyingGlassIcon,
+  PlusIcon,
+  ShareIcon,
+  StarIcon,
+} from "@heroicons/react/20/solid";
+import {
+  ArrowTrendingUpIcon,
+  Bars3Icon,
+  BellIcon,
+  FireIcon,
+  HomeIcon,
+  UserGroupIcon,
+  XMarkIcon,
+  InformationCircleIcon,
+  ChatBubbleBottomCenterTextIcon,
+} from "@heroicons/react/24/outline";
+
 
 const navigation = [
-  { name: 'Dashboard', icon: HomeIcon, href: '#', current: true },
-  { name: 'Team', icon: UsersIcon, href: '#', current: false },
-  { name: 'Projects', icon: FolderIcon, href: '#', current: false },
-  { name: 'Calendar', icon: CalendarIcon, href: '#', current: false },
-  { name: 'Documents', icon: InboxIcon, href: '#', current: false },
-  { name: 'Reports', icon: ChartBarIcon, href: '#', current: false },
-]
-const secondaryNavigation = [
-  { name: 'Website redesign', href: '#' },
-  { name: 'GraphQL API', href: '#' },
-  { name: 'Customer migration guides', href: '#' },
-  { name: 'Profit sharing program', href: '#' },
-]
+  { name: "Home", href: "#", icon: HomeIcon, current: true },
+  { name: "Tags", href: "/tags", icon: ArrowTrendingUpIcon, current: false },
+  { name: "Users", href: "/users", icon: UserGroupIcon, current: false },
+  { name: "Contact", href: "/contact", icon: ChatBubbleBottomCenterTextIcon, current: false },
+];
+
+const communities = [
+  { name: "React", href: "#" },
+  { name: "Flutter", href: "#" },
+  { name: "Next JS", href: "#" },
+  { name: "Android", href: "#" },
+  { name: "JavaScript", href: "#" },
+  { name: "Java", href: "#" },
+];
+
+
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
-  return (
-    <div className="flex flex-grow flex-col overflow-y-auto border-r border-gray-200 bg-white pt-5 pb-4">
+export default function Sidebar() {
 
-      <div className="mt-5 flex flex-grow flex-col">
-        <nav className="flex-1 space-y-8 bg-white px-2" aria-label="Sidebar">
-          <div className="space-y-1">
-            {navigation.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className={classNames(
-                  item.current ? 'bg-gray-100 text-gray-900' : 'text-gray-600 hover:bg-gray-50 hover:text-gray-900',
-                  'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                )}
+  return (
+    <>
+      <div className="min-h-full">
+        <div className="py-10">
+          <div className="mx-auto max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-12 lg:gap-8 lg:px-8">
+            <div className="hidden lg:col-span-3 lg:block xl:col-span-2">
+              <nav
+                aria-label="Sidebar"
+                className="sticky top-4 divide-y divide-gray-300"
               >
-                <item.icon
-                  className={classNames(
-                    item.current ? 'text-gray-500' : 'text-gray-400 group-hover:text-gray-500',
-                    'mr-3 flex-shrink-0 h-6 w-6'
-                  )}
-                  aria-hidden="true"
-                />
-                {item.name}
-              </a>
-            ))}
-          </div>
-          <div className="space-y-1">
-            <h3 className="px-3 text-sm font-medium text-gray-500" id="projects-headline">
-              Projects
-            </h3>
-            <div className="space-y-1" role="group" aria-labelledby="projects-headline">
-              {secondaryNavigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-600 hover:bg-gray-50 hover:text-gray-900"
-                >
-                  <span className="truncate">{item.name}</span>
-                </a>
-              ))}
+                <div className="space-y-1 pb-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className={classNames(
+                        item.current
+                          ? "bg-gray-200 text-gray-900"
+                          : "text-gray-700 hover:bg-gray-50",
+                        "group flex items-center px-3 py-2 text-sm font-medium rounded-md"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      <item.icon
+                        className={classNames(
+                          item.current
+                            ? "text-gray-500"
+                            : "text-gray-400 group-hover:text-gray-500",
+                          "flex-shrink-0 -ml-1 mr-3 h-6 w-6"
+                        )}
+                        aria-hidden="true"
+                      />
+                      <span className="truncate">{item.name}</span>
+                    </a>
+                  ))}
+                </div>
+                <div className="pt-10 pb-5">
+                  <p
+                    className="px-3 text-sm font-medium text-gray-500"
+                    id="communities-headline"
+                  >
+                    Popular Tags
+                  </p>
+                  <div
+                    className="mt-3 space-y-2"
+                    aria-labelledby="communities-headline"
+                  >
+                    {communities.map((community) => (
+                      <a
+                        key={community.name}
+                        href={community.href}
+                        className="group flex items-center rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
+                      >
+                        <span className="truncate">{community.name}</span>
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </nav>
             </div>
+            content here
           </div>
-        </nav>
-      </div>
-    </div>
-  )
+        </div>
+      </div>              
+    </>
+  );
 }
