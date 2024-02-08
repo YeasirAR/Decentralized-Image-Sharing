59f25bd5526993313ec9d35d183c662d267cdcb9
Yeasir Arafat <yeasirar@gmail.com>
2024-02-08 23:44:02 +0600

    minor changes

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -7,6 +7,7 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const user = {
   name: 'Chelsea Hagon',
+
   email: 'chelsea.hagon@example.com',
   imageUrl:
     'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
