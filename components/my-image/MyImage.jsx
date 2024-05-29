77595e39a07463e0a6ca005759ccfd1ab9dedfa7
Sayem Siam <sayemsiam198@gmail.com>
2024-05-30 00:00:05 +0600

    minor changes in share image and my image

--- components/my-image/MyImage.jsx
+++ components/my-image/MyImage.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 
-const ShareImage = () => {
+const MyImage = () => {
   return (
     <>
-      <div className="w-[130vh] p-4 mx-auto my-3 text-left bg-white border border-gray-200 shadow-lg shadow-purple-500/50 rounded-lg shadow sm:p-8 ">
-        <h5 className="mb-2 text-3xl font-bold text-gray-900 ">Image Name</h5>
-        <h3 className="mb-2 text-3xl font-bold text-gray-900 ">Block Id</h3>
-        <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-          Share With: <br />
-          <button
-            type="button"
-            class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white"
-          >
-            abc@xyz.com
-          </button>
-          <button
-            type="button"
-            class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white"
-          >
-            abc@xyz.com
-          </button>
-          <button
-            type="button"
-            class="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white"
-          >
-            abc@xyz.com
-          </button>
-        </p>
+      <div className=" grid grid-cols-2 gap-2">
+        <div className="w-[130vh] p-4 mx-auto my-3 text-left bg-white border border-gray-200 shadow-lg shadow-purple-500/50 rounded-lg shadow sm:p-8 ">
+          <h5 className="mb-2 text-3xl font-bold text-gray-900 ">Image Name</h5>
+          <h3 className="mb-2 text-3xl font-bold text-gray-900 ">Block Id</h3>
+          <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+            Share With: <br />
+            <button
+              type="button"
+              className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white"
+            >
+              abc@xyz.com
+            </button>
+            <button
+              type="button"
+              className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border-t border-b border-gray-900 hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white"
+            >
+              abc@xyz.com
+            </button>
+            <button
+              type="button"
+              className="px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white"
+            >
+              abc@xyz.com
+            </button>
+          </p>
+        </div>
         <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
           <a
             href="#"
@@ -39,11 +41,10 @@ const ShareImage = () => {
               </div>
             </div>
           </a>
-          
         </div>
       </div>
     </>
   );
 };
 
-export default ShareImage;
+export default MyImage;
