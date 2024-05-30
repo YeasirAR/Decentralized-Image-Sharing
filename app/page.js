786f0556c9b093888bffded6e00357b44b08365f
Yeasir Arafat <yeasirar@gmail.com>
2024-05-30 06:20:00 +0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,22 @@
-import Image from 'next/image'
-import Login from '../components/auth/login'
-import Signup from '../components/auth/signup'
-import Sidebar from '@/components/sidebar/sidebar'
+import LoginPage from "@/components/auth/login";
+import { cookies } from "next/headers";
+import { redirect } from 'next/navigation';
+import jwt from 'jsonwebtoken';
 
 export default function Home() {
-  var flag = true;
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+  if (token) {
+    const decodedToken = jwt.decode(token);
+    console.log(decodedToken.email);
+    redirect('/dashboard'); 
+  }
+  else{
+    redirect('/auth/login');
+  }
   return (
     <main>
-      {flag? <Login /> : <Signup />}
     </main>
   )
 }
+
